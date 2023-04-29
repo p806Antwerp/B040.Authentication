fe68e3c9201fa13b1023ef5701ee061b74601594
Web <p806antwerp@gmail.com>
2023-04-29 11:47:13 +0200

    Adresses an issue where swagger does not work because of one of the loading of the Bootstrap script.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -16,7 +16,7 @@ namespace B040.Authentication
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
