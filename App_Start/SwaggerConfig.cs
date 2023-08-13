cbecb469d9a41ee610575192e0e1e042da0316e7
Web <p806antwerp@gmail.com>
2023-08-13 21:14:49 +0200

    6297.17 Bring back to P806

--- App_Start/SwaggerConfig.cs
+++ App_Start/SwaggerConfig.cs
@@ -25,7 +25,8 @@ namespace B040.Authentication
                         // However, there may be situations (e.g. proxy and load-balanced environments) where this does not
                         // resolve correctly. You can workaround this by providing your own code to determine the root URL.
                         //
-                        c.RootUrl(req => modB040Config.Generic("API-ADDRESS"));
+                        //string url =  modB040Config.Generic("API-ADDRESS");
+                        //c.RootUrl(req => url);
 
                         // If schemes are not explicitly provided in a Swagger 2.0 document, then the scheme used to access
                         // the docs is taken as the default. If your API supports multiple schemes and you want to be explicit
