dc7209e623834003012b5ee221bfbf84860d6e65
p806Antwerp <102176895+p806Antwerp@users.noreply.github.com>
2025-09-27 11:07:55 +0200

    6322.06 Release 6322

---
+++ Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace B040.Authentication.Controllers
+{
+    public class PingController : ApiController
+    {
+        [HttpGet]
+        [Route("ping")]
+        public IHttpActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
